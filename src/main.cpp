0f6dcefe14f225a22dadba9f225cfa95ab81a876
PieVieRo <tymek24m@gmail.com>
2022-12-31 20:10:47 +0100

    we got textures working let's go!!!

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ int main() {
     const int HEIGHT = 720;
     InitWindow(WIDTH, HEIGHT,  TITLE);
 
+    Texture2D person = LoadTexture("assets/sprites/player.png");
     const int FPS = 60;
     SetTargetFPS(FPS);
 
@@ -16,10 +17,11 @@ int main() {
     }
     while(!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawFPS(0,0);
-        DrawCircleGradient(400, 300, 20, GOLD, DARKPURPLE);
-        DrawText("Hello World", WIDTH / 2 - 5, HEIGHT / 2 - 15, 30, BLACK);
+            ClearBackground(RAYWHITE);
+            DrawFPS(0,0);
+            DrawCircleGradient(400, 300, 20, GOLD, DARKPURPLE);
+            DrawTexture(person, 200, 500, WHITE);
+            DrawText("Hello World", WIDTH / 2 - 5, HEIGHT / 2 - 15, 30, BLACK);
         EndDrawing();
     }
     return 0;
