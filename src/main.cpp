f4d2ae287b5fc1daedf12a3b4463eea637e5aecc
PieVieRo <tymek24m@gmail.com>
2023-01-04 12:48:11 +0100

    my code works somehow idk how
    (probably some slavic IT magic voodoo stuff)

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include <raymath.h>
 #include <iostream>
 
-
 #include "player.hpp"
+#include "block.cpp"
 
 int main() {
     const char* TITLE = "Sokoban!!!";
@@ -13,24 +13,52 @@ int main() {
 
     const int FPS = 60;
     SetTargetFPS(FPS);
-    Player person(
-        (Vector2){0, 0},
-        LoadTexture("assets/sprites/player.png")
-        );
 
     if(!IsWindowReady()) {
         std::cout << "Window isn't ready :(" << std::endl;
         return -1;
     }
+
+    Block ground(LoadTexture("assets/sprites/ground.png"), true);
+    Block wall(LoadTexture("assets/sprites/wall.png"));
+    Block* test_level[10][10] {
+        &wall, &wall, &wall, &wall,  &wall,  &wall,  &wall,  &wall,  &wall,  &wall,
+        &wall, &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &wall,
+        &wall, &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &wall,
+        &wall, &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &wall,
+        &wall, &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &wall,
+        &wall, &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &wall,
+        &wall, &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &wall,
+        &wall, &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &wall,
+        &wall, &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &ground,  &wall,
+        &wall, &wall,  &wall,  &wall,  &wall,  &wall,  &wall,  &wall,  &wall,  &wall,
+    };
+
+    Player person(
+            (Vector2){0, 0},
+            LoadTexture("assets/sprites/player.png")
+            );
+
     while(!WindowShouldClose()) {
         // Drawing
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawFPS(0,0);
+
+            for(int x=0; x<10; x++) {
+                for(int y=0; y<10; y++) {
+                    if(test_level[x][y] != &ground)
+                        ground.draw(x,y);
+                    test_level[x][y]->draw(x,y);
+                }
+            }
             person.draw();
+
+            DrawFPS(0,0);
+
         EndDrawing();
         // Physics and stuff idk
         person.move();
+
     }
     return 0;
 }
