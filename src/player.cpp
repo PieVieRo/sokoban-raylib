f5602d297c3f7d3829a15ff291a36a2aabb187a9
Tymoteusz Moryto <tymek24m@gmail.com>
2023-03-09 21:48:13 +0100

    holy shit im so stupid
    collisions now work tho :)

--- src/player.cpp
+++ src/player.cpp
@@ -12,12 +12,13 @@ void Player::draw() {
 }
 
 void Player::move(Level* level) {
+
     if(IsKeyPressed(KEY_UP))  {
-        if(level->data[(int)(this->location.y-1*10 + this->location.x)]) return;
+        if(level->data[(int)((this->location.y-1)*10 + this->location.x)]) return;
         this->location.y -= 1;
     }
     if(IsKeyPressed(KEY_DOWN))    {
-        if(level->data[(int)(this->location.y+1*10 + this->location.x)]) return;
+        if(level->data[(int)((this->location.y+1)*10 + this->location.x)]) return;
         this->location.y += 1;
     }
     if(IsKeyPressed(KEY_RIGHT)) {
